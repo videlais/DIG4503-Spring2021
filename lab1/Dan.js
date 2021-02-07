0737e5edbda7c09e412ab7bd9726140d0d69c3b5
Dan Cox <daniel.57@gmail.com>
2021-02-06 22:17:24 -0500

    Created Lab 1 Files

---
+++ lab1/Dan.js
@@ -0,0 +1,9 @@
+import Person from './Person.js';
+
+class Dan extends Person {
+    constructor(name, color) {
+        super(name, color);
+    }
+}
+
+export default Dan;
---
+++ lab1/index.js
@@ -0,0 +1,4 @@
+import Dan from './Dan.js';
+
+const d = new Dan("Dan", "#FFFFFF");
+d.speak();
